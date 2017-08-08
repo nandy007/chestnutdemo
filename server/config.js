aaad0af376a32a8bbbe4ae964a6b4f7631bd8de8
huangnan <huangnan@nj.fiberhome.com.cn>
2017-08-08 14:26:52 +0800

    完善config说明

--- server/config.js
+++ server/config.js
@@ -24,6 +24,7 @@ module.exports = {
   port: 3001,
   rootPath: path.join(__dirname),
   database: database/*,
+  // 此处没配置sessionConfig使用默认存储，但是建议配置，请事先准备一个mysql数据库
   middleware: {
     sessionConfig: {
       key: 'SESSIONID',
