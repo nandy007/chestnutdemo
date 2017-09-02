9507ff4a53008a68b6ddbc595e1f571ddc99e551
nandy007 <nandy_wong@126.com>
2017-09-02 23:09:17 +0800

    添加x-forwarded-for反向代理信息设置规则

--- server_custom/config.js
+++ server_custom/config.js
@@ -11,10 +11,23 @@ const decode = function (str) {
 };
 
 const getClientIp = function (req) {
-	return req.headers['x-forwarded-for'] ||
-		req.connection.remoteAddress ||
+	return req.connection.remoteAddress ||
 		req.socket.remoteAddress ||
 		req.connection.socket.remoteAddress;
+};
+
+const getServerIp = function () {
+	var interfaces = require('os').networkInterfaces();
+	for (var devName in interfaces) {
+		var iface = interfaces[devName];
+		for (var i = 0; i < iface.length; i++) {
+			var alias = iface[i];
+			if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+				return alias.address;
+			}
+		}
+	}
+	return '';
 };
 
 const cookies = function (req) {
@@ -66,12 +79,32 @@ const rule3 = function (options) {
 	};
 }
 
+// 添加反向代理设置x-forwarded-for头信息
+const rule4 = function (options) {
+	return function (req, res, proxy) {
+		let opts = { target: 'http://127.0.0.1:8001', headers: {} };
+		//x-forwarded-for HTTP头一般格式如下:
+		//X-Forwarded-For: client, proxy1, proxy2, proxy3
+		const xIP = req.headers['x-forwarded-for'];
+		if (!xIP) {
+			const ip = getClientIp(req);
+			// 如果xIp不存在则代表本代理为一级代理，第一个数据应为客户端ip
+			if (ip) opts.headers['x-forwarded-for'] = ip;
+		}
+		// 无论本代理是不是一级代理，最终都要添加到最后
+		opts.headers['x-forwarded-for'] += ', ' + getServerIp();
+
+		// 通过代理指向分配的服务器
+		proxy.web(req, res, opts);
+	}
+};
+
 
 module.exports = {
 	"port": "3000",
 	//"rule": "sourcebalancer", // 此规则已经添加到chestnut-app的默认代理中
 	/*"rule": ,*/
-	rule: rule2,
+	rule: rule4,
 	servers: [
 		'http://127.0.0.1:3001',
 		'http://127.0.0.1:3002'
